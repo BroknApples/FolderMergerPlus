0b19fa95edf94635054f36ed1ae94009f94320d2
Brokn_Apples <trentons708@gmail.com>
2025-07-23 18:11:44 -0500

    My First Commit

---
+++ src/folder_merger_plus/app.hpp
@@ -0,0 +1,14 @@
+#ifndef APP_HPP
+#define APP_HPP
+
+
+class App {
+  private:
+
+  public:
+    App();
+    ~App();
+};
+
+
+#endif // APP_HPP
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "folder_merger_plus/app.hpp"
+#include <iostream>
+
+/**
+ * @brief Entry point of the program.
+ */
+int main() {
+  std::cout << "Hello World!\n";
+
+
+  return 0;
+}
